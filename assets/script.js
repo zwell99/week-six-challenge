23bc8df77e8cdab886231a671a7970408ada56b4
Zach Wellens <zachwellens990@gmail.com>
2023-05-11 15:35:06 -0400

    Wrote display functions

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,10 @@
 //  doing it this way seemed simpler than however I needed to resolve the jquery issue
 var searchFormEl = document.querySelector('#searchForm');
 var cityInputEl = document.querySelector('#city');
-//var prevSearchesEl = document.querySelector('#previousCities');
 var prevSearchesEl = $("#previousCities");
+
+var todayResults = $("#results").find("#today");
+var futureResults= $("#results").find("#future");
 
 
 var prevSearches = [];
@@ -21,7 +23,8 @@ function formSubmitHandler(event) {
 }
 
 function getCityWeather(name) {
-    var cityApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + name + "&limit=1&appid="; // appid?
+    var apiKey = "e0061d20649a643387d6b548aace1afa"; //For some reason this works on the first api call, but not the second
+    var cityApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + name + "&limit=1&appid=" + apiKey;
     var cityLat;
     var cityLon;
     fetch(cityApiUrl).then(function (response) {
@@ -38,7 +41,7 @@ function getCityWeather(name) {
     var temp = [];
     var wind = [];
     var humid = [];
-    var weatherApiUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?lat=" + cityLat + "&lon=" + cityLon + "&cnt=6&units=imperial&appid=";
+    var weatherApiUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?lat=" + cityLat + "&lon=" + cityLon + "&cnt=6&units=imperial&appid=" + apiKey;
     fetch(weatherApiUrl).then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
@@ -57,8 +60,34 @@ function getCityWeather(name) {
     console.log(temp);
     console.log(wind);
     console.log(humid);
-    fillTodayWeather();
-    fillWeekWeather();
+    fillTodayWeather(weatherSymbol[0], temp[0], wind[0], humid[0]);
+    fillWeekWeather(weatherSymbol, temp, wind, humid);
+}
+
+function fillTodayWeather(weatherSymbol, temp, wind, humid) {
+    var symbol = todayResults.find("#icon");
+    var temperature = todayResults.find("#temp");
+    var windSpeed = todayResults.find("#wind");
+    var humidity = todayResults.find("#humid");
+    symbol.text(weatherSymbol);
+    temperature.text("Temperature: " + temp + "F");
+    windSpeed.text("Wind Speed: " + wind + "mph");
+    humidity.text("Humidity: " + humid + "%");
+}
+
+function fillTodayWeather(weatherSymbol, temp, wind, humid) {
+  for (var i = 1; i < 6; i++) {
+    var elId = "day" + i;
+    var currentEl = futureResults.find(elId);
+    var symbol = currentEl.find("#icon");
+    var temperature = currentEl.find("#temp");
+    var windSpeed = currentEl.find("#wind");
+    var humidity = currentEl.find("#humid");
+    symbol.text(weatherSymbol[i]);
+    temperature.text("Temperature: " + temp[i] + "F");
+    windSpeed.text("Wind Speed: " + wind[i] + "mph");
+    humidity.text("Humidity: " + humid[i] + "%");
+  }
 }
 
 function createButton(name) {
